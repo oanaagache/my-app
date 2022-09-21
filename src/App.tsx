304fc1c324a21b2deadfe8886919ddad0815230b
Oana Agache <oana.luciana.agache@gmail.com>
2022-09-21 18:58:14 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
-import { Products } from "./components/Products/Products";
-import { Contact } from "./pages/Contact";
 import { CartProvider } from "./context/CartContext";
-import { Product } from "./components/Product/Product";
-import { Categories } from "./components/Product/Categories";
 import "./index.css";
+import AppRoutes from "./AppRoutes";
 
-function App() {
+export default function App() {
   return (
     <>
       <CartProvider>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/products/:uuid" element={<Product />} />
-        </Routes>
+        <AppRoutes />
       </CartProvider>
     </>
   );
 }
-
-export default App;
---
+++ src/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Products } from "./components/Products/Products";
+import { Contact } from "./pages/Contact";
+import { Product } from "./components/Product/Product";
+import { Categories } from "./components/Product/Categories";
+import "./index.css";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/products/:uuid" element={<Product />} />
+    </Routes>
+  );
+}
