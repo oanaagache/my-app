d609b8aa4183718502b01d9ced5772882706ea0d
Oana Agache <oana.luciana.agache@gmail.com>
2022-09-23 08:10:00 +0300

    Update

--- src/components/Product/CRUD/CreateProduct.tsx
+++ src/components/Product/CRUD/CreateProduct.tsx
@@ -1,9 +1,17 @@
 import { Form, Button } from "react-bootstrap";
 import { useState } from "react";
-import { useCreateProduct } from "../../../hooks/custom/useCreateProduct";
+//import { useCreateProduct } from "../../../hooks/custom/useCreateProduct";
 import { useCreateProductMutation } from "../../../hooks/query/useCreateProductMutation";
 import { useForm } from "react-hook-form";
 import { ICreateProductReq } from "../../../types/ICreateProductReq";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const createProductSchema = z.object({
+  name: z.string().min(3).max(10),
+  category: z.string().min(3).max(10),
+  price: z.string(),
+});
 
 export const CreateProduct = () => {
   const [name, setName] = useState("");
@@ -13,13 +21,16 @@ export const CreateProduct = () => {
   // const { isLoading, isError, createProduct, product } = useCreateProduct(); =>
   const { isLoading, mutate: createProduct, data } = useCreateProductMutation();
 
-  const { register, handleSubmit, reset } = useForm<ICreateProductReq>({
-    defaultValues: {
-      name: "",
-      category: "",
-      price: 1,
-    },
-  });
+  const { register, handleSubmit, reset, formState } =
+    useForm<ICreateProductReq>({
+      defaultValues: {
+        name: "",
+        category: "",
+        price: 1,
+      },
+      mode: "onBlur",
+      resolver: zodResolver(createProductSchema),
+    });
 
   const onSubmit = async (values: ICreateProductReq) => {
     createProduct(values, {
@@ -42,7 +53,12 @@ export const CreateProduct = () => {
           }}
         >
           <Form.Label>Name:</Form.Label>
-          <Form.Control type="Name" placeholder="Name" {...register("name")} />
+          <Form.Control
+            type="Name"
+            placeholder="Name"
+            // error={formState.errors?.name?.message}
+            {...register("name")}
+          />
         </Form.Group>
         <Form.Group
           className="mb-3"
@@ -54,6 +70,8 @@ export const CreateProduct = () => {
           <Form.Control
             type="Category"
             placeholder="Category"
+            //error="blabla"
+            error={formState.errors?.category?.message}
             {...register("category")}
           />
         </Form.Group>
@@ -67,6 +85,7 @@ export const CreateProduct = () => {
           <Form.Control
             type="Price"
             placeholder="Price"
+            // error={formState.errors?.price?.message}
             {...register("price")}
           />
         </Form.Group>
